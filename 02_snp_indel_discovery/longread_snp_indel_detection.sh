b7f2efd189ee34309a76827b39edaed3540be2db
Qi Bao <baoqinetwon@gmail.com>
2026-06-11 16:34:04 +0800

    Add ONT Clair3 SNP Indel calling workflow

--- 02_snp_indel_discovery/longread_snp_indel_detection.sh
+++ 02_snp_indel_discovery/longread_snp_indel_detection.sh
@@ -2,20 +2,20 @@
 set -euo pipefail
 
 # ============================================================
-# Long-read SNP/Indel detection using pbmm2 and DeepVariant
+# Long-read SNP/Indel detection
 # ============================================================
 # Workflow:
-#   1. Align PacBio HiFi reads to the reference genome using pbmm2
-#   2. Sort BAM during alignment
-#   3. Call SNPs and Indels using DeepVariant with the PACBIO model
+#   PacBio HiFi:
+#     1. Align HiFi reads using pbmm2 --preset HIFI
+#     2. Call SNPs/Indels using DeepVariant --model_type=PACBIO
 #
-# Example input:
-#   BMA_hifi.fastq.gz
+#   Oxford Nanopore Technologies (ONT):
+#     1. Align ONT reads using minimap2 -x map-ont
+#     2. Call SNPs/Indels using Clair3
 #
-# Example output:
-#   01_bam/BMA_pbmm.bam
-#   02_deepvariant/BMA.vcf.gz
+# Set READ_TYPE=HIFI or READ_TYPE=ONT before running.
 
+READ_TYPE=${READ_TYPE:-HIFI}      # HIFI or ONT
 THREADS=${THREADS:-52}
 
 SAMPLE=${SAMPLE:-BMA}
@@ -24,44 +24,86 @@ REF_MMI=${REF_MMI:-/public/home/baoqi/assembly/Duroc/Duroc.mmi}
 FASTQ=${FASTQ:-BMA_hifi.fastq.gz}
 
 BAM_DIR=${BAM_DIR:-01_bam}
-VCF_DIR=${VCF_DIR:-02_deepvariant}
+VCF_DIR=${VCF_DIR:-02_variant_calling}
 
-DEEPVARIANT_SIF=${DEEPVARIANT_SIF:-/public/software/singularity/docker-images/deepvariant-gpu.1.4.0.sif}
 SINGULARITY=${SINGULARITY:-/public/software/singularity/bin/singularity}
+DEEPVARIANT_SIF=${DEEPVARIANT_SIF:-/public/software/singularity/docker-images/deepvariant-gpu.1.4.0.sif}
+
+# Clair3 settings for ONT SNP/Indel calling
+CLAIR3=${CLAIR3:-run_clair3.sh}
+CLAIR3_MODEL=${CLAIR3_MODEL:-/path/to/clair3/ont/model}
+CLAIR3_PLATFORM=${CLAIR3_PLATFORM:-ont}
 
 mkdir -p ${BAM_DIR} ${VCF_DIR}
 
-# ------------------------------------------------------------
-# 1. Align HiFi reads with pbmm2
-# ------------------------------------------------------------
+case ${READ_TYPE} in
 
-pbmm2 align \
-    --preset HIFI \
-    -j ${THREADS} \
-    --sort \
-    --sort-threads ${THREADS} \
-    --sample ${SAMPLE} \
-    ${REF_MMI} \
-    ${FASTQ} \
-    ${BAM_DIR}/${SAMPLE}_pbmm.bam
+    HIFI|HiFi|hifi)
+        echo "Running PacBio HiFi SNP/Indel detection for ${SAMPLE}"
 
-# ------------------------------------------------------------
-# 2. SNP/Indel calling with DeepVariant GPU
-# ------------------------------------------------------------
-# Use GPU by adding '--nv' to singularity exec.
+        # ------------------------------------------------------------
+        # 1. Align HiFi reads with pbmm2
+        # ------------------------------------------------------------
 
-ls ${BAM_DIR}/*.bam | while read -r bam
- do
-    file=$(basename ${bam})
-    sample=${file%%.*}
+        pbmm2 align \
+            --preset HIFI \
+            -j ${THREADS} \
+            --sort \
+            --sort-threads ${THREADS} \
+            --sample ${SAMPLE} \
+            ${REF_MMI} \
+            ${FASTQ} \
+            ${BAM_DIR}/${SAMPLE}_pbmm.bam
 
-    echo "Calling SNPs/Indels for ${sample} from ${file}"
+        samtools index -@ ${THREADS} ${BAM_DIR}/${SAMPLE}_pbmm.bam
 
-    ${SINGULARITY} exec --nv --network=none -n ${DEEPVARIANT_SIF} \
-        run_deepvariant \
-        --model_type=PACBIO \
-        --ref=${REF_FA} \
-        --reads=${bam} \
-        --output_vcf=${VCF_DIR}/${sample}.vcf.gz \
-        --num_shards=${THREADS}
- done
+        # ------------------------------------------------------------
+        # 2. SNP/Indel calling with DeepVariant GPU
+        # ------------------------------------------------------------
+        # Use GPU by adding '--nv' to singularity exec.
+
+        ${SINGULARITY} exec --nv --network=none -n ${DEEPVARIANT_SIF} \
+            run_deepvariant \
+            --model_type=PACBIO \
+            --ref=${REF_FA} \
+            --reads=${BAM_DIR}/${SAMPLE}_pbmm.bam \
+            --output_vcf=${VCF_DIR}/${SAMPLE}.hifi.deepvariant.vcf.gz \
+            --num_shards=${THREADS}
+        ;;
+
+    ONT|ont)
+        echo "Running ONT SNP/Indel detection for ${SAMPLE}"
+
+        # ------------------------------------------------------------
+        # 1. Align ONT reads with minimap2
+        # ------------------------------------------------------------
+
+        minimap2 \
+            -ax map-ont \
+            -t ${THREADS} \
+            ${REF_FA} \
+            ${FASTQ} \
+        | samtools sort \
+            -@ ${THREADS} \
+            -o ${BAM_DIR}/${SAMPLE}.ont.minimap2.bam
+
+        samtools index -@ ${THREADS} ${BAM_DIR}/${SAMPLE}.ont.minimap2.bam
+
+        # ------------------------------------------------------------
+        # 2. SNP/Indel calling with Clair3
+        # ------------------------------------------------------------
+
+        ${CLAIR3} \
+            --bam_fn=${BAM_DIR}/${SAMPLE}.ont.minimap2.bam \
+            --ref_fn=${REF_FA} \
+            --threads=${THREADS} \
+            --platform=${CLAIR3_PLATFORM} \
+            --model_path=${CLAIR3_MODEL} \
+            --output=${VCF_DIR}/${SAMPLE}.ont.clair3
+        ;;
+
+    *)
+        echo "ERROR: unsupported READ_TYPE=${READ_TYPE}. Use READ_TYPE=HIFI or READ_TYPE=ONT." >&2
+        exit 1
+        ;;
+esac
