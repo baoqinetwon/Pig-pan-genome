a2a8c553a2ba09c0cb9b1ac41c0225b85d3a75fc
Qi Bao <baoqinetwon@gmail.com>
2026-06-11 21:52:44 +0800

    Replace hardcoded panel number with PGVRP in SNP-SV merge script

--- 13_panel_imputation/03_merge_snp_sv_by_chr.sh
+++ 13_panel_imputation/03_merge_snp_sv_by_chr.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 # ============================================================
 # 03_merge_snp_sv_by_chr.sh
-# Merge chromosome-level SNP and SV VCFs
+# Merge chromosome-level SNP and SV VCFs for PGVRP
 # ============================================================
 
 THREADS=20
-SNP_VCF=01_snp_qc/panel_3376.genome.snps.reheadered.vcf.gz
-SV_VCF=02_sv_qc/panel_2337.chr1-18.sv.only_bi.vcf.gz
-SAMPLE_LIST=2337_panel.sample.sorted.txt
+SNP_VCF=01_snp_qc/PGVRP.genome.snps.reheadered.vcf.gz
+SV_VCF=02_sv_qc/PGVRP.chr1-18.sv.only_bi.vcf.gz
+SAMPLE_LIST=PGVRP_panel.sample.sorted.txt
 OUTDIR=03_chr_snp_sv
 
 mkdir -p ${OUTDIR}
@@ -18,7 +18,7 @@ for CHR in {1..18}
 do
     echo "Merging SNP and SV for chromosome ${CHR}"
 
-    # Extract biallelic SNPs from final panel samples
+    # Extract biallelic SNPs from PGVRP samples
     bcftools view \
         --threads ${THREADS} \
         -v snps \
@@ -26,30 +26,30 @@ do
         -S ${SAMPLE_LIST} \
         -r ${CHR} \
         ${SNP_VCF} \
-        -Oz -o ${OUTDIR}/panel_2337.chr${CHR}.snp.vcf.gz
+        -Oz -o ${OUTDIR}/PGVRP.chr${CHR}.snp.vcf.gz
 
-    tabix -p vcf ${OUTDIR}/panel_2337.chr${CHR}.snp.vcf.gz
+    tabix -p vcf ${OUTDIR}/PGVRP.chr${CHR}.snp.vcf.gz
 
     # Extract filtered SVs
     bcftools view \
         --threads ${THREADS} \
         -r ${CHR} \
         ${SV_VCF} \
-        -Oz -o ${OUTDIR}/panel_2337.chr${CHR}.sv.vcf.gz
+        -Oz -o ${OUTDIR}/PGVRP.chr${CHR}.sv.vcf.gz
 
-    tabix -p vcf ${OUTDIR}/panel_2337.chr${CHR}.sv.vcf.gz
+    tabix -p vcf ${OUTDIR}/PGVRP.chr${CHR}.sv.vcf.gz
 
     # Merge SNP and SV VCFs
     bcftools concat \
         --threads ${THREADS} \
         -a -d snps \
-        ${OUTDIR}/panel_2337.chr${CHR}.snp.vcf.gz \
-        ${OUTDIR}/panel_2337.chr${CHR}.sv.vcf.gz \
-        -Oz -o ${OUTDIR}/panel_2337.chr${CHR}.snp.sv.vcf.gz
+        ${OUTDIR}/PGVRP.chr${CHR}.snp.vcf.gz \
+        ${OUTDIR}/PGVRP.chr${CHR}.sv.vcf.gz \
+        -Oz -o ${OUTDIR}/PGVRP.chr${CHR}.snp.sv.vcf.gz
 
     bcftools sort \
-        ${OUTDIR}/panel_2337.chr${CHR}.snp.sv.vcf.gz \
-        -Oz -o ${OUTDIR}/panel_2337.chr${CHR}.snp.sv.sorted.vcf.gz
+        ${OUTDIR}/PGVRP.chr${CHR}.snp.sv.vcf.gz \
+        -Oz -o ${OUTDIR}/PGVRP.chr${CHR}.snp.sv.sorted.vcf.gz
 
-    tabix -p vcf ${OUTDIR}/panel_2337.chr${CHR}.snp.sv.sorted.vcf.gz
+    tabix -p vcf ${OUTDIR}/PGVRP.chr${CHR}.snp.sv.sorted.vcf.gz
 done
